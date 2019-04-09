2f4b52a897082c2b4af72fe98fdbd894d95ced13
Kish Siva <rodes88@msn.com>
2019-04-09 23:04:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,12 @@ def reverse_each_word(sent1)
   i = 0 
   arr = []
   sent1=sent1.split("\s")
-  sent1.collect do |word| 
+  sent1.collect! do |word| 
     word = word.reverse
-    arr[i] = word 
+    #arr[i] = word 
+    #i+=1
   end 
-  arr.join(" ")
+ # arr.join(" ")
   
 end 
 
